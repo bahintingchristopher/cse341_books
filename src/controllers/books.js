d8143b7594375b392029b57c760153445a14a329
Christopher Bahinting <bahintingchristopher@gmail.com>
2026-09-05 19:01:06 +0800

    update the return messages for each query

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -4,7 +4,10 @@ import { getAllBooks, getBookById } from '../models/books.js';
 const getBooksHandler = async (req, res) => {
   try {
     const books = await getAllBooks();
-    return res.status(200).json(books);
+    return res.status(200).json( {
+        message: 'Books retrieved successfully',
+        data: books
+    });
   } catch (error) {
     console.error('GET /books failed:', error.message);
     return res.status(500).json({ message: 'Internal server error' });
@@ -21,7 +24,10 @@ const getBookByIdHandler = async (req, res) => {
       return res.status(404).json({ message: 'Book not found' });
     }
 
-    return res.status(200).json(book);
+    return res.status(200).json( {
+        message: ' Certain Book retrieved successfully',
+        data: book
+    });
   } catch (error) {
     console.error('GET /books/:id failed:', error.message);
     return res.status(500).json({ message: 'Internal server error' });
